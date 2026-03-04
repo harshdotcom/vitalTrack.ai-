4ca1666c5a9fd2f9628eaf8f011f156fd966eec5
Jawwad Umar <jawwadumar99@gmail.com>
2026-03-05 05:17:27 +0530

    refactoring

--- backend/utility/helper.go
+++ backend/utility/helper.go
@@ -1,10 +1,5 @@
 package utility
 
-import (
-	"encoding/json"
-	"vita-track-ai/models"
-)
-
 // Helper function to safely get strings from claims
 func GetClaim(key string, claims map[string]interface{}) string {
 
@@ -14,22 +9,4 @@ func GetClaim(key string, claims map[string]interface{}) string {
 		}
 	}
 	return ""
-}
-
-func ParseResponse(respBody []byte) (*models.MedicalReport, error) {
-	// Step 1: Unmarshal outer JSON
-	var outer struct {
-		JSON string `json:"json"`
-	}
-	if err := json.Unmarshal(respBody, &outer); err != nil {
-		return nil, err
-	}
-
-	// Step 2: Unmarshal inner JSON string
-	var report models.MedicalReport
-	if err := json.Unmarshal([]byte(outer.JSON), &report); err != nil {
-		return nil, err
-	}
-
-	return &report, nil
 }
