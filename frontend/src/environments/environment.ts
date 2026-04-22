a135115dacfd3d5ba3109172de1b2bd4e6ba875a
Jawwad Umar <jawwadumar99@gmail.com>
2026-04-23 02:21:22 +0530

    for deploy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api/v1',
+  apiUrl: 'https://vitaltrack-ai.onrender.com/api/v1',
   emailVerificationEnabled: true,   //  OTP-Flow Set to true when DISABLE_EMAIL_FLOW=false on backend
 };
